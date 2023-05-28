44a26ca9e6e6d9d01c2fc6c8fb428226c2e73486
SHEN <122678304+shensuz@users.noreply.github.com>
2023-05-28 16:13:31 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: 'zsshengsu@gmail.com',
   link: 'https://periduv.com',
   description: 'This gonna be an awesome world.',
-  lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
